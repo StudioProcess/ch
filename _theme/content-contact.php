81328775092ab0f703065f972ce8a2d3b247b85b
RESL <moritz@resl.xyz>
2016-07-20 11:47:05 +0200

    added section for 'represented by' on contact page

--- _theme/content-contact.php
+++ _theme/content-contact.php
@@ -1,5 +1,5 @@
 <div id="contact">
-	<section class="contact"><?php echo ch_get_content( 11 ); ?></section>
+	<section class="contact"><?php echo ch_get_content( 11 ); ?><section class="represented-by"><?php echo ch_get_content( 1117 ); ?></section></section>
 	<section class="imprint"><?php echo ch_get_content( 9 ); ?><section class="design"><?php echo ch_get_content( 187 ); ?></section></section>
 	<div class="backlink"><a  href="/">(go back)</a></div>
 </div>
