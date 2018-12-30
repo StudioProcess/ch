bc3874f9414667bc9aa56e04ff4fb79faf650ed3
Martin Leopold <m@martinleopold.com>
2018-12-30 17:29:37 +0100

    Analytics: Anonymize IPs

--- _theme/footer.php
+++ _theme/footer.php
@@ -22,6 +22,7 @@
 		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 		  ga('create', 'UA-54332019-1', 'auto');
+		  ga('set', 'anonymizeIp', true);
 		  ga('send', 'pageview');
 		</script>
 
